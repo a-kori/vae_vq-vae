ece53e4225576042a4c04db665596d79fbb114b9
Ramneek2 <104074322+Ramneek2@users.noreply.github.com>
2024-06-08 20:30:56 +0200

    Update networks.py with batch norm and dropout

--- networks.py
+++ networks.py
@@ -2,22 +2,28 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class Encoder(nn.Module):
     def __init__(self, input_channels, latent_dim):
         super(Encoder, self).__init__()
-        self.conv1 = nn.Conv2d(input_channels, 32, kernel_size=4, stride=2, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1)
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1)
-        self.fc1 = nn.Linear(128 * 4 * 4, 512)
+        self.conv1 = nn.Conv2d(input_channels, 32, kernel_size=4, stride=2, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.fc1 = nn.Linear(128 * 4 * 4, 512, bias=False)
+        self.dropout = nn.Dropout(0.4)
         self.fc2_mean = nn.Linear(512, latent_dim)
-        self.fc2_logvar = nn.Linear(512, latent_dim)
+        self.fc2_logvar = nn.Linear(512, latent_dim) 
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
         x = x.view(x.size(0), -1)
         h = F.relu(self.fc1(x))
+        h = self.dropout(h)
         mean = self.fc2_mean(h)
         logvar = self.fc2_logvar(h)
         return mean, logvar
@@ -25,34 +31,20 @@ class Encoder(nn.Module):
 class Decoder(nn.Module):
     def __init__(self, latent_dim):
         super(Decoder, self).__init__()
-        self.fc1 = nn.Linear(latent_dim, 512)
-        self.fc2 = nn.Linear(512, 128 * 4 * 4)
-        self.deconv1 = nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1)
-        self.deconv2 = nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1)
-        self.deconv3 = nn.ConvTranspose2d(32, 3, kernel_size=4, stride=2, padding=1)
+        self.fc1 = nn.Linear(latent_dim, 512, bias=False)
+        self.fc2 = nn.Linear(512, 128 * 4 * 4, bias=False)
+        self.deconv1 = nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.deconv2 = nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.deconv3 = nn.ConvTranspose2d(32, 3, kernel_size=4, stride=2, padding=1)  # Keep bias here
 
     def forward(self, z):
         z = F.relu(self.fc1(z))
         z = F.relu(self.fc2(z))
         z = z.view(z.size(0), 128, 4, 4)  # Reshape tensor
-        z = F.relu(self.deconv1(z))
-        z = F.relu(self.deconv2(z))
+        z = F.relu(self.bn1(self.deconv1(z)))
+        z = F.relu(self.bn2(self.deconv2(z)))
         z = torch.sigmoid(self.deconv3(z))
         return z
 
-class VAE(nn.Module):
-    def __init__(self, input_channels, latent_dim):
-        super(VAE, self).__init__()
-        self.encoder = Encoder(input_channels, latent_dim)
-        self.decoder = Decoder(latent_dim)
-
-    def reparameterize(self, mean, logvar):
-        std = torch.exp(0.5 * logvar)
-        eps = torch.randn_like(std)
-        return mean + eps * std
-
-    def forward(self, x):
-        mean, logvar = self.encoder(x)
-        z = self.reparameterize(mean, logvar)
-        x_reconstructed = self.decoder(z)
-        return x_reconstructed, mean, logvar
