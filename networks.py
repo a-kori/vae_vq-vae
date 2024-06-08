5631b07a9602755b37aa08d07d26dd66b25e3391
anastasiia.korzhylova <anastasiia.korzhylova@tu-dortmund.de>
2024-06-08 13:25:53 +0200

    Adjusted training to networks

--- networks.py
+++ networks.py
@@ -1,10 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
-from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
-import matplotlib.pyplot as plt
 
 class Encoder(nn.Module):
     def __init__(self, input_channels, latent_dim):
@@ -17,20 +13,15 @@ class Encoder(nn.Module):
         self.fc2_logvar = nn.Linear(512, latent_dim)
 
     def forward(self, x):
-        #print(x.shape)
         x = F.relu(self.conv1(x))
-        #print(x.shape)
         x = F.relu(self.conv2(x))
-        #print(x.shape)
         x = F.relu(self.conv3(x))
-        #print(x.shape)
         x = x.view(x.size(0), -1)
-        #print(x.shape)  # Flatten the tensor
         h = F.relu(self.fc1(x))
-        #print(h.shape)
         mean = self.fc2_mean(h)
         logvar = self.fc2_logvar(h)
         return mean, logvar
+
 class Decoder(nn.Module):
     def __init__(self, latent_dim):
         super(Decoder, self).__init__()
@@ -41,19 +32,12 @@ class Decoder(nn.Module):
         self.deconv3 = nn.ConvTranspose2d(32, 3, kernel_size=4, stride=2, padding=1)
 
     def forward(self, z):
-        #print(z.shape)
         z = F.relu(self.fc1(z))
-        #print(z.shape)
         z = F.relu(self.fc2(z))
-        #print(z.shape)
         z = z.view(z.size(0), 128, 4, 4)  # Reshape tensor
-        #print(z.shape)
         z = F.relu(self.deconv1(z))
-        #print(z.shape)
         z = F.relu(self.deconv2(z))
-        #print(z.shape)
         z = torch.sigmoid(self.deconv3(z))
-        #print(z)
         return z
 
 class VAE(nn.Module):
@@ -68,16 +52,7 @@ class VAE(nn.Module):
         return mean + eps * std
 
     def forward(self, x):
-        #print(x.shape)
         mean, logvar = self.encoder(x)
         z = self.reparameterize(mean, logvar)
-        #print(z.shape)
         x_reconstructed = self.decoder(z)
         return x_reconstructed, mean, logvar
-
-def loss_function(reconstructed_x, x, mean, logvar):
-    #print(reconstructed_x.shape)
-    #print(x.shape)
-    BCE = F.binary_cross_entropy(reconstructed_x, x, reduction='sum')
-    KLD = -0.5 * torch.sum(1 + logvar - mean.pow(2) - logvar.exp())
-    return BCE + KLD
