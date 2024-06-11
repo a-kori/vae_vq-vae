0970eeef15a9d021132c63650089da7d5c46bd37
Ramneek2 <104074322+Ramneek2@users.noreply.github.com>
2024-06-11 12:52:41 +0200

    change convolutional layer size to 64

--- VAE/networks.py
+++ VAE/networks.py
@@ -5,9 +5,9 @@ import torch.nn.functional as F
 class Encoder(nn.Module):
     def __init__(self, input_channels, latent_dim):
         super(Encoder, self).__init__()
-        self.conv1 = nn.Conv2d(input_channels, 32, kernel_size=4, stride=2, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(32)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(input_channels, 64, kernel_size=4, stride=2, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=4, stride=2, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(64)
         self.conv3 = nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1, bias=False)
         self.bn3 = nn.BatchNorm2d(128)
