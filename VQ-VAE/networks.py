2fd9120ff58f440f0f6e680730e9dfb69aa23c8e
Ramneek2 <104074322+Ramneek2@users.noreply.github.com>
2024-06-29 12:13:52 +0200

    added vq vae to main

---
+++ VQ-VAE/networks.py
@@ -0,0 +1,137 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from datasets import create_datasets
+
+#Observation: Higher model complexity causes overfitting    
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        identity = x
+        out = self.relu(self.bn1(self.conv1(x)))
+        out = self.bn1(self.conv2(out))
+        out += identity
+        return out    
+
+class Encoder(nn.Module):
+    def __init__(self, input_channels, emb_dim):
+        super(Encoder, self).__init__()
+        self.conv1 = nn.Conv2d(input_channels, 128, kernel_size=4, stride=2, padding=1)
+        self.bn1 = nn.BatchNorm2d(128)
+        self.conv2 = nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1)
+        self.bn2 = nn.BatchNorm2d(256)
+        self.resblock1 = ResidualBlock(256,256)
+        self.resblock2 = ResidualBlock(256,256)
+        self.conv3 = nn.Conv2d(256, emb_dim, kernel_size=3, padding=1)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.resblock1(x)
+        x = self.resblock2(x)
+        x = self.conv3(x)
+        return x
+
+class Decoder(nn.Module):
+    def __init__(self, output_channels, emb_dim):
+        super(Decoder, self).__init__()
+        self.deconv1 = nn.Conv2d(emb_dim, 256, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(256)
+        self.resblock1 = ResidualBlock(256,256)
+        self.resblock2 = ResidualBlock(256,256)
+        self.deconv2 = nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.deconv3 = nn.ConvTranspose2d(128, output_channels, kernel_size=4, stride=2, padding=1)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.deconv1(x)))
+        x = self.resblock1(x)
+        x = self.resblock2(x)
+        x = F.relu(self.bn2(self.deconv2(x)))
+        x = torch.sigmoid(self.deconv3(x))
+        return x    
+    
+class VectorQuantizer(nn.Module):
+    def __init__(self, emb_dim, n_embed, beta):
+        super(VectorQuantizer, self).__init__()
+        self.emb_dim = emb_dim
+        self.n_embed = n_embed
+        self.embedding = nn.Embedding(self.n_embed, self.emb_dim)
+        self.embedding.weight.data.normal_(mean=0, std=1/self.n_embed)
+        self.beta = beta
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        
+    def forward(self, encoderout):
+        encoderout = encoderout.permute(0, 2, 3, 1).contiguous()
+        flat_input = encoderout.view(-1, self.emb_dim)
+
+        # Compute distances using torch.cdist, cdist requires shape [n,m,b] so unsqueeze both vectors
+        distances = torch.cdist(flat_input.unsqueeze(0), self.embedding.weight.unsqueeze(0),p = 2).squeeze(0)
+        
+        # chose closest embedding vector 
+        encoding_indices = torch.argmin(distances, dim=1).unsqueeze(1)
+        closest_embedding = self.embedding(encoding_indices).view(encoderout.shape)
+        
+        # Loss calculation
+        #codebook loss adapts embedding vectors to encoder output
+        codebook_loss = F.mse_loss(closest_embedding, encoderout.detach())
+        #commitment loss adapts encoder output to embeddings
+        commitment_loss = F.mse_loss(encoderout, closest_embedding.detach())
+        loss = codebook_loss + self.beta * commitment_loss
+
+        # Straight-through estimator
+        # Observation: Straight through estimator is causing a gradual increase in vq loss
+        closest_embedding = encoderout + (closest_embedding - encoderout).detach()
+        closest_embedding = encoderout.permute(0, 3, 1, 2).contiguous()
+        
+        return closest_embedding, loss, codebook_loss, commitment_loss
+        
+class VQVAE(nn.Module):
+    def __init__(self, input_channels, emb_dim, n_embed, beta):
+        super(VQVAE, self).__init__()
+        self.encoder = Encoder(input_channels,emb_dim)
+        self.decoder = Decoder(input_channels,emb_dim)
+        self.vectorquantizer = VectorQuantizer(emb_dim, n_embed, beta)
+
+    def forward(self, x):
+        z = self.encoder(x)
+        z_quant, loss,embedding_loss,commitment_loss = self.vectorquantizer.forward(z)
+        x_reconstructed = self.decoder(z_quant)
+        return x_reconstructed, loss,embedding_loss,commitment_loss
+#debugging purposes
+'''
+def main():
+    # Configuration
+    input_channels = 3
+    emb_dim = 64
+    n_embed = 512
+    commitment_cost = 0.25
+    batch_size = 128  # Just for demonstration, usually larger for training
+    image_size = 32  # CIFAR-10 image size
+    
+    # Create the VQ-VAE model
+    model = VQVAE(input_channels, emb_dim, n_embed, commitment_cost).to('cuda' if torch.cuda.is_available() else 'cpu')
+    dataset_name = 'CIFAR10'
+    train_loader, test_loader = create_datasets(dataset_name, batch_size)
+
+    images, labels = next(iter(train_loader))  # Fetch the next batch
+    
+    
+    # Move to GPU if available
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    images = images.to(device)
+    model = model.to(device)
+    
+    reconstructed_images, loss, embedding_loss, commitment_loss = model(images)
+    
+if __name__ == "__main__":
+    main()
+    
+    '''
---
+++ VQ-VAE/plotting.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as plt
+import torch
+
+def plot_loss_lr(num_epochs, train_losses, test_losses, learning_rates):
+    '''
+    Plots the Train Loss, Test Loss and Learning Rate development.
+    '''
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4))
+    fig.suptitle('Train Loss & Test Loss & LR')
+    epochs = range(1, num_epochs + 1)
+
+    # Plot Train & Test losses
+    ax1.set_title('Train & Test losses')
+    ax1.set_xlabel('Epoch')
+    ax1.plot(epochs, train_losses, label='Train Loss', color='C0')
+    ax1.plot(epochs, test_losses, label='Test Loss', color='C1')
+    ax1.legend()
+
+    # Plot Learning Rate over time
+    ax2.set_title('Learning Rate over time')
+    ax2.set_xlabel('Epoch')
+    ax2.plot(epochs, learning_rates, label='Learning Rate', color='C2')
+
+    fig.tight_layout()
+    plt.show()
+
+
+def plot_loss_components(reconstruction_losses, kl_losses):
+    '''
+    Plots the development of the 2 loss components: the reconstruction loss and the KL divergence loss.
+    '''
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4))
+    fig.suptitle('Loss Components')
+
+    # Convert lists to tensors
+    reconstruction_losses = torch.tensor(reconstruction_losses)
+    kl_losses = torch.tensor(kl_losses)
+
+    # Transfer to CPU and convert to NumPy arrays
+    reconstruction_losses = reconstruction_losses.cpu().numpy()
+    kl_losses = kl_losses.cpu().numpy()
+
+    # Plot reconstruction loss
+    ax1.set_title('Reconstruction Loss')
+    ax1.set_xlabel('Batch')
+    ax1.set_ylabel('Reconstruction Loss')
+    ax1.plot(range(len(reconstruction_losses)), reconstruction_losses, label='Reconstruction Loss', color='C0')
+
+    # Plot KL divergence loss
+    ax2.set_title('KL Divergence Loss')
+    ax2.set_xlabel('Batch')
+    ax2.set_ylabel('KL Divergence Loss')
+    ax2.plot(range(len(kl_losses)), kl_losses, label='KL Divergence Loss', color='C1')
+
+    fig.tight_layout()
+    plt.show()
