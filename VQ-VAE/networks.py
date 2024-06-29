c54241280e4e9cf6afa69c222ade1e43560f3cc9
Ramneek2 <104074322+Ramneek2@users.noreply.github.com>
2024-06-29 12:03:54 +0200

    changes to network and vector quant, good recon

--- VQ-VAE/networks.py
+++ VQ-VAE/networks.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 from datasets import create_datasets
 
-    
+#Observation: Higher model complexity causes overfitting    
 class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(ResidualBlock, self).__init__()
@@ -23,9 +23,9 @@ class ResidualBlock(nn.Module):
 class Encoder(nn.Module):
     def __init__(self, input_channels, emb_dim):
         super(Encoder, self).__init__()
-        self.conv1 = nn.Conv2d(input_channels, 256, kernel_size=4, stride=2, padding=1)
-        self.bn1 = nn.BatchNorm2d(256)
-        self.conv2 = nn.Conv2d(256, 256, kernel_size=4, stride=2, padding=1)
+        self.conv1 = nn.Conv2d(input_channels, 128, kernel_size=4, stride=2, padding=1)
+        self.bn1 = nn.BatchNorm2d(128)
+        self.conv2 = nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1)
         self.bn2 = nn.BatchNorm2d(256)
         self.resblock1 = ResidualBlock(256,256)
         self.resblock2 = ResidualBlock(256,256)
@@ -46,9 +46,9 @@ class Decoder(nn.Module):
         self.bn1 = nn.BatchNorm2d(256)
         self.resblock1 = ResidualBlock(256,256)
         self.resblock2 = ResidualBlock(256,256)
-        self.deconv2 = nn.ConvTranspose2d(256, 256, kernel_size=4, stride=2, padding=1)
-        self.bn2 = nn.BatchNorm2d(256)
-        self.deconv3 = nn.ConvTranspose2d(256, output_channels, kernel_size=4, stride=2, padding=1)
+        self.deconv2 = nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.deconv3 = nn.ConvTranspose2d(128, output_channels, kernel_size=4, stride=2, padding=1)
 
     def forward(self, x):
         x = F.relu(self.bn1(self.deconv1(x)))
@@ -59,50 +59,46 @@ class Decoder(nn.Module):
         return x    
     
 class VectorQuantizer(nn.Module):
-    def __init__(self, emb_dim, n_embed, commitment_cost):
+    def __init__(self, emb_dim, n_embed, beta):
         super(VectorQuantizer, self).__init__()
         self.emb_dim = emb_dim
         self.n_embed = n_embed
         self.embedding = nn.Embedding(self.n_embed, self.emb_dim)
-        self.embedding.weight.data.uniform_(-1/self.n_embed, 1/self.n_embed)
-        self.commitment_cost = commitment_cost
+        self.embedding.weight.data.normal_(mean=0, std=1/self.n_embed)
+        self.beta = beta
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         
-    def forward(self, z):
-        z = z.permute(0, 2, 3, 1).contiguous()
-        input_shape = z.shape
-        flat_input = z.view(-1, self.emb_dim)
+    def forward(self, encoderout):
+        encoderout = encoderout.permute(0, 2, 3, 1).contiguous()
+        flat_input = encoderout.view(-1, self.emb_dim)
 
         # Compute distances using torch.cdist, cdist requires shape [n,m,b] so unsqueeze both vectors
         distances = torch.cdist(flat_input.unsqueeze(0), self.embedding.weight.unsqueeze(0),p = 2).squeeze(0)
         
         # chose closest embedding vector 
         encoding_indices = torch.argmin(distances, dim=1).unsqueeze(1)
-        encodings = torch.zeros(encoding_indices.shape[0], self.n_embed, device=self.device)
+        closest_embedding = self.embedding(encoding_indices).view(encoderout.shape)
         
-        # Scatter encodings
-        encodings.scatter_(1, encoding_indices, 1)
-
-        # Quantize and unflatten
-        z_quant = torch.matmul(encodings, self.embedding.weight).view(input_shape)
-
         # Loss calculation
-        z_quant_detached = z_quant.detach()
-        codebook_loss = F.mse_loss(z_quant, z.detach())
-        commitment_loss = F.mse_loss(z, z_quant_detached)
-        loss = codebook_loss + self.commitment_cost * commitment_loss
+        #codebook loss adapts embedding vectors to encoder output
+        codebook_loss = F.mse_loss(closest_embedding, encoderout.detach())
+        #commitment loss adapts encoder output to embeddings
+        commitment_loss = F.mse_loss(encoderout, closest_embedding.detach())
+        loss = codebook_loss + self.beta * commitment_loss
 
         # Straight-through estimator
-        z_quant = z + (z_quant - z).detach()
-        z_quant = z_quant.permute(0, 3, 1, 2).contiguous()
-        return z_quant, loss, codebook_loss, commitment_loss
+        # Observation: Straight through estimator is causing a gradual increase in vq loss
+        closest_embedding = encoderout + (closest_embedding - encoderout).detach()
+        closest_embedding = encoderout.permute(0, 3, 1, 2).contiguous()
+        
+        return closest_embedding, loss, codebook_loss, commitment_loss
         
 class VQVAE(nn.Module):
-    def __init__(self, input_channels, emb_dim, n_embed, commitment_cost):
+    def __init__(self, input_channels, emb_dim, n_embed, beta):
         super(VQVAE, self).__init__()
         self.encoder = Encoder(input_channels,emb_dim)
         self.decoder = Decoder(input_channels,emb_dim)
-        self.vectorquantizer = VectorQuantizer(emb_dim, n_embed, commitment_cost)
+        self.vectorquantizer = VectorQuantizer(emb_dim, n_embed, beta)
 
     def forward(self, x):
         z = self.encoder(x)
