7359540f74066cb4686a1641e21c84744cf9b22d
a-kori <nastya.korzhylova@gmail.com>
2024-06-29 09:23:58 +0200

    VQ-VAE implementation

---
+++ VAE/vq_vae.py
@@ -0,0 +1,82 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.utils import weight_norm
+
+class VectorQuantizer(nn.Module):
+    def __init__(self, num_embeddings, embedding_dim, commitment_cost):
+        super(VectorQuantizer, self).__init__()
+        self.embedding_dim = embedding_dim
+        self.num_embeddings = num_embeddings
+        self.commitment_cost = commitment_cost
+
+        self.embeddings = nn.Embedding(self.num_embeddings, self.embedding_dim)
+        self.embeddings.weight.data.uniform_(-1/self.num_embeddings, 1/self.num_embeddings)
+
+    def forward(self, inputs):
+        # Convert inputs from B x C x H x W to BHW x C
+        inputs = inputs.permute(0, 2, 3, 1).contiguous()
+        input_shape = inputs.shape
+        flat_input = inputs.view(-1, self.embedding_dim)
+
+        # Calculate distances
+        distances = (torch.sum(flat_input**2, dim=1, keepdim=True) 
+                     + torch.sum(self.embeddings.weight**2, dim=1)
+                     - 2 * torch.matmul(flat_input, self.embeddings.weight.t()))
+
+        # Encoding
+        encoding_indices = torch.argmin(distances, dim=1).unsqueeze(1)
+        encodings = torch.zeros(encoding_indices.shape[0], self.num_embeddings, device=inputs.device)
+        encodings.scatter_(1, encoding_indices, 1)
+
+        # Quantize and unflatten
+        quantized = torch.matmul(encodings, self.embeddings.weight).view(input_shape)
+
+        # Loss
+        e_latent_loss = F.mse_loss(quantized.detach(), inputs)
+        q_latent_loss = F.mse_loss(quantized, inputs.detach())
+        loss = q_latent_loss + self.commitment_cost * e_latent_loss
+
+        # Straight-through estimator
+        quantized = inputs + (quantized - inputs).detach()
+
+        return quantized.permute(0, 3, 1, 2).contiguous(), loss
+
+class Encoder(nn.Module):
+    def __init__(self, input_channels, embedding_dim):
+        super(Encoder, self).__init__()
+        self.conv1 = nn.Conv2d(input_channels, 64, kernel_size=4, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1)
+        self.conv3 = nn.Conv2d(128, embedding_dim, kernel_size=4, stride=2, padding=1)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = self.conv3(x)
+        return x
+
+class Decoder(nn.Module):
+    def __init__(self, embedding_dim, output_channels):
+        super(Decoder, self).__init__()
+        self.deconv1 = nn.ConvTranspose2d(embedding_dim, 128, kernel_size=4, stride=2, padding=1)
+        self.deconv2 = nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1)
+        self.deconv3 = nn.ConvTranspose2d(64, output_channels, kernel_size=4, stride=2, padding=1)
+
+    def forward(self, x):
+        x = F.relu(self.deconv1(x))
+        x = F.relu(self.deconv2(x))
+        x = torch.sigmoid(self.deconv3(x))
+        return x
+
+class VQVAE(nn.Module):
+    def __init__(self, input_channels, embedding_dim, num_embeddings, commitment_cost):
+        super(VQVAE, self).__init__()
+        self.encoder = Encoder(input_channels, embedding_dim)
+        self.vq = VectorQuantizer(num_embeddings, embedding_dim, commitment_cost)
+        self.decoder = Decoder(embedding_dim, input_channels)
+
+    def forward(self, x):
+        z = self.encoder(x)
+        z_q, vq_loss = self.vq(z)
+        x_reconstructed = self.decoder(z_q)
+        return x_reconstructed, vq_loss
