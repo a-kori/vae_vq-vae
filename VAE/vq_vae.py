bffeb9a3782541236fd6567fafefff7bd1cabc55
a-kori <nastya.korzhylova@gmail.com>
2024-06-29 11:13:16 +0200

    Tested model reconstructions

--- VAE/vq_vae.py
+++ VAE/vq_vae.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.nn.utils import weight_norm
 
 class VectorQuantizer(nn.Module):
     def __init__(self, num_embeddings, embedding_dim, commitment_cost):
